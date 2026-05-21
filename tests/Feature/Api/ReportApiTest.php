08a559e1105ee8c5a5ae0fb6db1994fdb7c9d03a
Joaquin Ruiz <47424155+JoaquinRuiz7@users.noreply.github.com>
2026-05-21 12:50:58 +0200

    test fetch reports with report count for reported user

--- tests/Feature/Api/ReportApiTest.php
+++ tests/Feature/Api/ReportApiTest.php
@@ -39,4 +39,32 @@ class ReportApiTest extends TestCase
             ->assertJsonCount(10, 'data')
             ->assertJsonPath('meta.total', 10);
     }
+
+    public function test_get_reports_with_report_count_for_reported(): void
+    {
+        $country = Country::factory()->create();
+
+        $users = User::factory()
+            ->count(3)
+            ->create([
+                'country_id' => $country->id,
+            ]);
+
+        Report::factory()
+            ->count(10)
+            ->create([
+                'user_id' => fn() => $users->random()->id,
+                'reported_user_id' => fn(array $attributes) => $users
+                    ->where('id', '!=', $attributes['user_id'])
+                    ->random()
+                    ->id,
+            ]);
+
+        $response = $this->getJson('/api/reports?withReportCountForReportedUser=1');
+        $response->assertJsonPath('data.0.reportedUserTotalReports', fn($value) => !is_null($value));
+        $response
+            ->assertOk()
+            ->assertJsonCount(10, 'data')
+            ->assertJsonPath('meta.total', 10);
+    }
 }
