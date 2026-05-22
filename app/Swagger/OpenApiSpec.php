763afb7a6416dc3f5f8b6fcf79547fd3015ad836
Joaquin Ruiz <47424155+JoaquinRuiz7@users.noreply.github.com>
2026-05-22 02:14:11 +0200

    Documentation title change (#16)
    * Users rest API (#1)
    
    * countries table migration
    
    * User index paginated
    
    * User index tests
    
    * fetch user by id
    
    * added registered to user response
    
    * Get user posts, tests are still missing for this endpoint
    
    * test for get user posts
    
    * Added last login at
    
    * user daily activity
    
    * Get posts (#2)
    
    * fetch posts
    
    * get post comments
    
    * Get comments endpoint (#3)
    
    * Reports endpoints (#4)
    
    * Renaming methods to keep consistency
    
    * report endpoint done, still missing tests
    
    * bugfix status filter
    
    * test get reports
    
    * test fetch reports with report count for reported user
    
    * reports summary
    
    * get report summary test
    
    * Security rest api (#5)
    
    * Get security logs
    
    * get blocked ips
    
    * Adjusted security log model
    
    * Get blocked phone done
    
    * get global security settings
    
    * Statistics endpoints (#6)
    
    * overview stats
    
    * use report status enum on test
    
    * use Illuminate\Http\Response for status code responses
    
    * get geographic distribution
    
    * viral post endpoint
    
    * added date filter on viral posts calculation
    
    * get user growth with date ranges
    
    * check for user existance
    
    * format code
    
    * swagger and open api set up
    
    * API documentation with swagger and open api (#7)
    
    * stats api documented
    
    * comments api documentation
    
    * post api documentation
    
    * documentation report count
    
    * security endpoints documented
    
    * all endpoints documented
    
    * pint config and code format (#8)
    
    * reverted handler option to default laravel (#9)
    
    * README file for app setup (#10)
    
    * Tests workflow config (#11)
    
    * test workflow configuration
    
    * changed php version to 8.1
    
    * rename to ci
    
    * remove .yml from name
    
    * Adjust seeders entities amount (#14)
    
    * adjust seeders amount
    
    * docks api title

--- app/Swagger/OpenApiSpec.php
+++ app/Swagger/OpenApiSpec.php
@@ -4,9 +4,9 @@ namespace App\Swagger;
 
 /**
  * @OA\Info(
- *     title="Your API Title",
+ *     title="Xlife admin backend",
  *     version="1.0.0",
- *     description="API Description"
+ *     description="Backend API designed for administrators to retrieve data."
  * )
  *
  * @OA\Server(
